ed2953910a13e1e821247f1f13755d7bd5332b7f
Gordon Carrie <dev0@trekix.net>
2009-09-16 18:25:19 +0000

    Initial revision

---
+++ src/biglo.c
@@ -0,0 +1,50 @@
+/*
+ - biglo.c --
+ -	Print a sequence of values with bigger steps at low values.
+ -
+   Copyright (c) 2009 Gordon D. Carrie
+   All rights reserved.
+
+   Please send feedback to dev0@trekix.net
+  
+   $Revision: $ $Date: $
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    char *cmd = argv[0], *lo_s, *hi_s, *N_s;
+    double lo, hi;
+    int n, N;
+
+    if (argc != 4) {
+	fprintf(stderr, "Usage: %s lo hi n\n", cmd);
+	exit(1);
+    }
+    lo_s = argv[1];
+    hi_s = argv[2];
+    N_s = argv[3];
+    if (sscanf(lo_s, "%lf", &lo) != 1) {
+	fprintf(stderr, "%s: expected float value for lo, got %s\n", cmd, lo_s);
+	exit(1);
+    }
+    if (sscanf(hi_s, "%lf", &hi) != 1) {
+	fprintf(stderr, "%s: expected float value for hi, got %s\n", cmd, hi_s);
+	exit(1);
+    }
+    if (sscanf(N_s, "%d", &N) != 1) {
+	fprintf(stderr, "%s: expected float value for n, got %s\n", cmd, N_s);
+	exit(1);
+    }
+
+    /* Uniform steps in log => values differ by a constant factor */
+    for (n = 0; n < N; n++) {
+	printf("%f ", lo + log(n + 1) / log(N) * (hi - lo));
+    }
+    printf("\n");
+
+    return 0;
+}
