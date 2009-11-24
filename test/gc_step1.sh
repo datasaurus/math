4604538242f7f7bdaea002a195f32397a9b7b0e8
Gordon Carrie <dev0@trekix.net>
2009-11-24 23:10:50 +0000

    Test script for step function

---
+++ test/gc_step1.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+#
+#-	step1.sh --
+#-		Test "geog step" function.
+#-
+#	Copyright (c) 2009 Gordon D. Carrie
+#	All rights reserved
+#
+#	Please send feedback to dev0@trekix.net
+#
+#	$Revision: $ $Date: $
+#
+########################################################################
+
+# This script compares output from geog with output from
+# geod, stored in file geod.out, which was produced as follows:
+#
+# radians per degree
+# rad_deg=0.01745329251994329576
+#
+# Earth radius = a
+# eval `geod -le | awk '/sphere/ {print $2}'`
+# export a
+#
+#    d_m=`echo "scale=17;$d_deg * $a * $rad_deg" | bc`
+#    echo $lat $lon $dir $d_m | geod -f '%.6f' +ellps=sphere \
+#	    | (read lat2 lon2 back
+#		printf "$f" $lon $lat $dir $d_deg $lon2 $lat2) \
+#	    | sed -e 's/-180/ 180/' -e 's/-0/ 0/' >> geod.out
+#
+# geod is part of proj4.
+# cf. http://trac.osgeo.org/proj
+
+# Set RM to : in environment to save temporary files.
+RM=${RM:-'rm -f'}
+
+PATH=$PWD/src:$PATH
+export PATH
+
+ANGLE_UNIT=DEGREE
+export ANGLE_UNIT
+
+if ! (cd src;make) > /dev/null
+then
+    echo "make failed"
+    exit 1
+fi
+
+rm -f geog.out
+f="(%5.1f %5.1f) dir %3.0f go %3.0f deg => (%10.5f %10.5f)\n"
+for lon in -179 -1 0 1 45 89 91 135 179 180 181 225 270 359
+do
+    for lat in -89 -45 -1 0 1 45 89
+    do
+	for dir in -270 -225 -135 -90 -45 0 45 90 135 180 225 270 360
+	do
+	    for d_deg in 0 10 80 100 170 190
+	    do
+		geog step $lon $lat $dir $d_deg \
+		    | (read lon2 lat2
+			    printf "$f" $lon $lat $dir $d_deg $lon2 $lat2) \
+			| sed -e 's/-180/ 180/' -e 's/-0/ 0/' >> geog.out
+	    done
+	done
+    done
+done
+
+if gunzip -c geod.out.gz | cmp geog.out -
+then
+    echo SUCCESS
+else
+    echo FAIL: geog.out and geod.out differ
+fi
+
+$RM geog.out
