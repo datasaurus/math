4ce29d8989b356637b5c95f08effe86a387caa5f
Gordon Carrie <dev0@trekix.net>
2009-12-11 19:49:42 +0000

    Changed nomenclature for angle vice geog interface

--- test/elev_wrap1.sh
+++ test/elev_wrap1.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
 #
-#- plat1.sh --
-#-	This test application tests conversion functions defined in geog_lib.c.
-#-	Specifically, it tests the plat (principal latitude function).
+#- angle_wrapelev.sh --
+#-	This test application tests conversion functions defined in angle_lib.c.
+#-	Specifically, it tests the Angle_WrapElev.
 #-
 # Copyright (c) 2009 Gordon D. Carrie
 # All rights reserved
 #
 # Please send feedback to dev0@trekix.net
 #
-# $Revision: 1.4 $ $Date: 2009/11/10 20:01:55 $
+# $Revision: 1.5 $ $Date: 2009/11/25 20:53:33 $
 #
 ########################################################################
 
 echo "
-plat1.sh --
+angle_wrapelev.sh --
 
-This test application tests conversion functions defined in geog_lib.c.
-Specifically, it tests the plat (principal latitude function).
-See geog_lib (3) and geog (1) for more information.
+This test application tests conversion functions defined in angle_lib.c.
+Specifically, it tests the Angle_WrapElev.
+See angle_lib (3) and angle (1) for more information.
 
-It reads latitudes from a local input file and puts them into
-the range -90 <= lat <= 90
+It reads elevations from a local input file and puts them into the range [-90, 90).
 
 Usage suggestions:
-./plat1.sh 2>&1 | less
+./angle_wrapelev.sh 2>&1 | less
 To save temporary files:
-env RM=: ./plat1.sh 2>&1 | less
+env RM=: ./angle_wrapelev.sh 2>&1 | less
 
 Copyright (c) 2009 Gordon D. Carrie
 All rights reserved
@@ -46,7 +45,7 @@ export PATH=$PWD/src:$PATH
 
 export ANGLE_UNIT="DEGREE"
 
-# Test input.  Columns: longitude reference_longitude geog_plat_result
+# Test input.  Columns: longitude reference_longitude angle_evel_wrap_result
 cat > input << END
  -370.0  -10.0
  -360.0    0.0
@@ -72,11 +71,11 @@ cat > input << END
   370.0   10.0
 END
 
-# For each line of input, give first and second column to 'geog plat'.
+# For each line of input, give first and second column to 'angle evel_wrap'.
 # Compare result with third column.
 awk '{print $1}' input | while read l
 do
-    printf '%7.1f%7.1f\n' $l `geog latn $l`
+    printf '%7.1f%7.1f\n' $l `angle elev_wrap $l`
 done | if diff input -
 then
     echo test1 produced good output
